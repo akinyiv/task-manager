7ffda98087b6dadd4013c40676ffd0a871217a8c
akinyiv <akinyivee10@gmail.com>
2022-09-19 14:59:33 +0300

    task manager

---
+++ frontend/src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div>
+            <NavLink to="/" activeClassName="is-active" exact={true}>Task 
+            </NavLink>
+            <NavLink to="/createuser" activeClassName="is-active">Create User</NavLink>
+            <NavLink to="/users" activeClassName="is-active"> User</NavLink>
+
+        </div>
+    );
+};
+
+export default Header;
--- frontend/src/routers/Approuter.js
+++ frontend/src/routers/Approuter.js
@@ -3,10 +3,12 @@ import {BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import CreateUser from '../components/createuser';
 import Users from '../components/users';
 import Task from '../components/task';
+import Header from '../components/header';
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
+            < Header />
             <Routes>
                 <Route exact path="/" element={<Task />} />
                 <Route path="/createuser" element={<CreateUser />} />
