b846159dc445efab808df4c96fbb5143960d26be
Velma Akinyi <akinyivee10@gmail.com>
2022-10-03 11:30:00 +0300

    add axios feature for backend requests

--- frontend/src/routers/Approuter.js
+++ frontend/src/routers/Approuter.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route } from 'react-router-dom';
-import CreateUser from '../components/createuser';
-import Users from '../components/users';
-import Task from '../components/task';
-import Header from '../components/header';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CreateUser from "../components/createuser";
+import Users from "../components/users";
+import Header from "../components/header";
 
 const AppRouter = () => {
-    return (
-        <BrowserRouter>
-        <div>
-            < Header />
-            <Switch>
-                <Route exact path="/" component={Task} />
-                <Route path="/createuser" component={CreateUser} />
-                <Route path="/users" component={Users} />
-            </Switch>
-        </div>
-            
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Users} />
+          <Route path="/createuser" component={CreateUser} />
+          <Route path="/users" component={Users} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
